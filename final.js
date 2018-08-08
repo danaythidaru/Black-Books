bd2a8f047a81431db6555d081491fcce5fedbc62
eyevent <eyevent@USSEAGirls21.ey.net>
2018-08-08 11:29:40 -0700

    ui

---
+++ final.js
@@ -0,0 +1,10 @@
+function changeContent(1, content1, 2, content2,)
+{
+	var contentChange1 = document.getElementById(1);
+	var contentChange2 = document.getElementById(2);
+
+
+	contentChange1.innerHTML = content2;
+	contentChange2.innerHTML = content1;
+
+}
